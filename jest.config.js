d2d5b67e303e52985bdbb6e09b5f90d0e1ac0cac
Y-Dharvik <96490499+Y-Dharvik@users.noreply.github.com>
2024-08-12 11:58:57 +0530

    Setup pre-commit hooks (#2)
    * setup pre-commit hooks
    
    * setup pre-commit hooks
    
    * removed jsx and tsx form linting

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
-  testMatch: ['**/dist/__tests__/**/*.test.js'],
-  testPathIgnorePatterns: ['<rootDir>/__tests__/'],
-  preset: "ts-jest",
-  testEnvironment: "node",
-  transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
-  },
-};
+	testMatch: ['**/dist/__tests__/**/*.test.js'],
+	testPathIgnorePatterns: ['<rootDir>/__tests__/'],
+	preset: "ts-jest",
+	testEnvironment: "node",
+	transform: {
+	  "^.+.tsx?$": ["ts-jest",{}],
+	},
+  };
